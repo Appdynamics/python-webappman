c2494001725d11fb3657f4d39b29df7188d7e4ed
Andrew Udvare <audvare@gmail.com>
2013-11-21 12:55:48 -0800

    Bump version and update change log

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='WebAppManager',
-    version='0.1.4',
+    version='0.1.5',
     author='Andrew Udvare',
     author_email='audvare@gmail.com',
     packages=['webappman'],
